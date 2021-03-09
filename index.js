8354e9ef00da61cb9bd7a4649ab7259bb5cc8222
stella4545 <stellachepchumbah@gmail.com>
2021-03-09 07:51:20 +0300

    javascript assignment

---
+++ index.js
@@ -0,0 +1,6 @@
+var fruits=["melon","strawberries","apples","mangoes","kiwi"];
+console.log(fruits[0])
+console.log(fruits[1])
+console.log(fruits[2])
+console.log(fruits[3])
+console.log(fruits[4])
